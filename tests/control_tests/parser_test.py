2f3d629180552579fb256111a475f93e32043e30
Anthony Francis <centaur@logicalrobotics.com>
2026-04-24 02:43:19 -0400

    Lintifix.

--- tests/control_tests/parser_test.py
+++ tests/control_tests/parser_test.py
@@ -7,8 +7,7 @@ from lrh.control.parser import parse_markdown_file, parse_markdown_text
 
 class TestControlParser(unittest.TestCase):
     def test_parse_markdown_text_frontmatter_and_body(self) -> None:
-        parsed = parse_markdown_text(
-            """---
+        parsed = parse_markdown_text("""---
 id: SAMPLE
 contributors: []
 items:
@@ -20,8 +19,7 @@ status: active
 # Body
 
 Hello.
-"""
-        )
+""")
 
         self.assertEqual(parsed.frontmatter["id"], "SAMPLE")
         self.assertEqual(parsed.frontmatter["contributors"], [])
